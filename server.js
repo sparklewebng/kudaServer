a4ae1998e99da9de00dd86180640c88c87ec2d32
makinmoni247 <makinmoni247@gmail.com>
2024-08-08 11:44:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "makinmoni247@gmail.com";
-const pass = "zusxabzqbuqlxyis";
+//const pass = "zusxabzqbuqlxyis";
 
 // Middleware
 app.use(express.json());
