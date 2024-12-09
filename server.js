f658b97a5174f1f203122f579d643a8913ab8592
kudahelpdeskng <kudahelpdeskng@gmail.com>
2024-12-09 08:10:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "makinmoni247@gmail.com";
-//const pass = "zusxabzqbuqlxyis";
-// 10 nov
+const pass = "zusxabzqbuqlxyis";
+// 13 jan
 
 // Middleware
 app.use(express.json());
